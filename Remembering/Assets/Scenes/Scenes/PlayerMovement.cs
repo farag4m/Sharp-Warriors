565ed1d28cb535ed175640e9f11090a0f393e495
Niftalem <niftalem.kassa@mnsu.edu>
2024-10-09 10:53:47 -0500

    Working walking footsteps
    Has echo's when near walls

--- Remembering/Assets/Scenes/Scenes/PlayerMovement.cs
+++ Remembering/Assets/Scenes/Scenes/PlayerMovement.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using AK.Wwise;
 
 [RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
@@ -11,54 +10,89 @@ public class PlayerMovement : MonoBehaviour
     public float runSpeed = 12f;
     public float jumpPower = 7f;
     public float gravity = 10f;
+    //public CharacterController characterController;
+    public float stepInterval = 0.3f;
+    private float stepTimer = 0f;
+
+    [Header("Wwise Events")]
+    public AK.Wwise.Event FootstepEvent;
+
     public float lookSpeed = 2f;
     public float lookXLimit = 45f;
-    public float defaultHeight = 2f;
-    public float crouchHeight = 1f;
-    public float crouchSpeed = 3f;
-
-    private Vector3 moveDirection = Vector3.zero;
-    private float rotationX = 0;
-    private CharacterController characterController;
-
-    public AK.Wwise.Event footstepEvent; // Reference to the Wwise footstep event
-    public float stepDistance = 1.0f; // Distance the character must move to trigger a footstep sound
-
-    private Vector3 lastFootstepPosition;
-    private bool canMove = true;
 
 
-}
+    Vector3 moveDirection = Vector3.zero;
+    float rotationX = 0;
 
-private bool canMove = true;
+    public bool canMove = true;
 
+    ////Wwise
+    //private bool footstepPlaying = false;
+    //private float lastFootstepTime = 0;
+
+
+    CharacterController characterController;
     void Start()
     {
         characterController = GetComponent<CharacterController>();
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-        lastFootstepPosition = transform.position;
+        //lastFootstepTime = Time.time;
     }
 
     void Update()
     {
+
+        #region Handles Movment
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         Vector3 right = transform.TransformDirection(Vector3.right);
 
-        float distanceMoved = Vector3.Distance(transform.position, lastFootstepPosition);
-
-        if (distanceMoved >= stepDistance)
-        {
-            PlayFootstepAudio();
-            lastFootstepPosition = transform.position;
-        }
-
+        // Press Left Shift to run
         bool isRunning = Input.GetKey(KeyCode.LeftShift);
         float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
         float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
         float movementDirectionY = moveDirection.y;
         moveDirection = (forward * curSpeedX) + (right * curSpeedY);
 
+        //if (!footstepPlaying)
+        //{
+        //    FootstepEvent.Post(gameObject);
+        //    lastFootstepTime = Time.time;
+        //    footstepPlaying = true;
+        //}
+        //else
+        //{
+
+        //    if (Time.time - lastFootstepTime > 0.5f)
+        //    {
+        //        footstepPlaying = false;
+        //    }
+
+        //}
+
+        if (Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0)
+        {
+            stepTimer += Time.deltaTime;
+            if (stepTimer >= stepInterval)
+            {
+                FootstepEvent.Post(gameObject);
+                stepTimer = 0;
+            }
+        }
+
+        if (Input.GetAxis("Vertical") == 0 && Input.GetAxis("Horizontal") == 0)
+        {
+            stepTimer = 0;
+        }
+
+        //if (Input.GetAxis("Vertical") == 0 && Input.GetAxis("Horizontal") == 0)
+        //{ 
+        //    footstepPlaying = false;
+        //}
+
+        #endregion
+
+        #region Handles Jumping
         if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
         {
             moveDirection.y = jumpPower;
@@ -73,20 +107,9 @@ private bool canMove = true;
             moveDirection.y -= gravity * Time.deltaTime;
         }
 
-        if (Input.GetKey(KeyCode.R) && canMove)
-        {
-            characterController.height = crouchHeight;
-            walkSpeed = crouchSpeed;
-            runSpeed = crouchSpeed;
+        #endregion
 
-        }
-        else
-        {
-            characterController.height = defaultHeight;
-            walkSpeed = 6f;
-            runSpeed = 12f;
-        }
-
+        #region Handles Rotation
         characterController.Move(moveDirection * Time.deltaTime);
 
         if (canMove)
@@ -95,17 +118,11 @@ private bool canMove = true;
             rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
             playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
             transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+
+            
+
+
+            #endregion
         }
     }
-public void PlayFootstepAudio()
-{
-    if (footstepEvent != null)
-    {
-        footstepEvent.Post(gameObject);
-    }
-    else
-    {
-        Debug.LogWarning("Footstep event is not assigned.");
-    }
-}   
 }
